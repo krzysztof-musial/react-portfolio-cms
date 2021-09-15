c08c42fe231cade6fe50b18fdebcc612b4dcff5e
Krzysztof Musial <krzysztofpiotrmusial@gmail.com>
2021-09-16 00:24:47 +0200

    configured router and added views

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+// Components
+import Home from './views/Home'
+import Login from './views/Login'
 
 export default function App() {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <main className="w-full min-h-screen">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Router>
+    </main>
   )
 }
