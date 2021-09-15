c54c456f9822d222e83f9decb7bbec6f08127c75
Krzysztof Musial <krzysztofpiotrmusial@gmail.com>
2021-09-16 01:59:36 +0200

    created Login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 // Components
 import Home from './views/Home'
 import Login from './views/Login'
 
 export default function App() {
   return (
-    <main className="w-full min-h-screen">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-        </Switch>
-      </Router>
-    </main>
+    <Router>
+      <main className="w-full min-h-screen">
+        <div className="w-full bg-gray-900 text-white text-xs px-2 py-1 flex justify-between">
+          <div className="flex space-x-2">
+            <Link to="/" className="hover:underline">Home</Link>
+            <Link to="/login" className="hover:underline">Login</Link>
+          </div>
+          <p>Not logged in.</p>
+        </div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+          </Switch>
+      </main>
+    </Router>
   )
 }
