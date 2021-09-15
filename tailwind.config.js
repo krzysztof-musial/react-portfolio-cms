8e58f8a610998b63763b1f3a0d364efcf7ef5c1f
Krzysztof Musial <krzysztofpiotrmusial@gmail.com>
2021-09-16 00:07:16 +0200

    initial commit /w router and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class',
+  theme: {
+      fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+          serif: ['Merriweather', 'serif'],
+          mono: ['Source Code Pro', 'monospace']
+      },
+      extend: {
+          colors: {
+              blueGray: colors.blueGray,
+              coolGray: colors.coolGray,
+              trueGray: colors.trueGray,
+              warmGray: colors.warmGray,
+              orange: colors.orange,
+              amber: colors.amber,
+              lime: colors.lime,
+              emerald: colors.emerald,
+              teal: colors.teal,
+              cyan: colors.cyan,
+              sky: colors.sky,
+              violet: colors.violet,
+              fuchsia: colors.fuchsia,
+              rose: colors.rose,
+          }
+      },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
