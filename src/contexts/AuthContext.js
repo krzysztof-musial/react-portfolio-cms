5216ddd39009f322701e80fa05edb21c3eb0493d
Krzysztof Musial <krzysztofpiotrmusial@gmail.com>
2021-09-16 23:14:28 +0200

    refactored AuthContext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,11 +1,7 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, createContext } from "react"
 import { auth } from "../firebase"
 
-const AuthContext = React.createContext("")
-
-// export function useAuth() {
-//   return useContext(AuthContext)
-// }
+const AuthContext = createContext("")
 
 const AuthProvider = ({ children }) => {
     const [currentUser , setCurrentUser] = useState()
