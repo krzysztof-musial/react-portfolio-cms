3caca387d011fdd04f7b07d88fafb50b914d9a07
Krzysztof Musial <krzysztofpiotrmusial@gmail.com>
2021-09-26 07:14:35 +0200

    files cleanup

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { AuthProvider } from "./contexts/AuthContext"
 import { ThemeContext } from "./contexts/ThemeContext"
 // Components
-import GuestRoute from './components/GuestRoute'
-import PrivateRoute from './components/PrivateRoute'
-// import Navigation from './components/Navigation'
+import { GuestRoute, PrivateRoute } from "./components/Routes"
 import Home from './views/Home'
 import Login from "./views/Login"
 import Admin from "./views/Admin"
@@ -20,14 +18,12 @@ export default function App() {
     <AuthProvider>
       <Router>
         <main className="w-full min-h-screen flex flex-col bg-white text-black dark:bg-gray-900 dark:text-white">
-          {/* Nav */}
-          {/* <Navigation /> */}
           {/* Content */}
           <Switch>
             <Route exact path="/" component={Home} />
             <GuestRoute path="/login" component={Login} />
-            <PrivateRoute path="/admin" component={Admin} />
             <Route path="/id/:id" component={Project}/>
+            <PrivateRoute path="/admin" component={Admin} />
           </Switch>
         </main>
       </Router>
---
+++ src/components/Routes.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react"
+import { Route, Redirect } from "react-router-dom"
+import { AuthContext } from "../contexts/AuthContext"
+
+function GuestRoute({ component: Component, ...rest }) {
+  const { currentUser } = useContext(AuthContext)
+
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        return !currentUser ? <Component {...props} /> : <Redirect to="/admin" />
+      }}
+    ></Route>
+  )
+}
+
+function PrivateRoute({ component: Component, ...rest }) {
+    const { currentUser } = useContext(AuthContext)
+  
+    return (
+      <Route
+        {...rest}
+        render={props => {
+          return currentUser ? <Component {...props} /> : <Redirect to="/login" />
+        }}
+      ></Route>
+    )
+}
+
+export { GuestRoute, PrivateRoute }
