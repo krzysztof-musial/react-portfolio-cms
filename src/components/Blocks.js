c32ab8d0669a97d1028c9fb9a159bb2206a8a642
Krzysztof Musial <krzysztofpiotrmusial@gmail.com>
2021-09-29 02:35:08 +0200

    blockeditor changes, not final

--- src/components/Blocks.js
+++ src/components/Blocks.js
@@ -117,46 +117,48 @@ function HeadingEditor(props) {
 
 // Layout
 function EditorLayout(props) {
+    const [showSettings, setShowSettings] = useState(false)
+
     return (
         <div>
-            <div className="w-full flex flex-col space-y-8">
+            <div className="relative" onMouseEnter={() => setShowSettings(true)} onMouseLeave={() => setShowSettings(false)}>
                 {/* Block settings */}
-                <div className="px-4 sm:px-8">
-                    <div className="p-2 rounded-lg border border-gray-200 flex space-x-2">
+                {showSettings &&
+                <div className="z-40 w-min absolute -left-4 top-0">
+                    <div className="p-2 rounded-lg bg-indigo-600 flex space-x-2">
                         <div className="flex flex-col items-center">
                             <button onClick={() => props.moveBlock(props.index, 'up')}>
                                 <svg className="fill-current text-gray-400 hover:text-gray-900 w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M12 9.5L7 14.5H17L12 9.5Z" />
                                 </svg>
                             </button>
+                            <button onClick={() => props.setShowEditor(true)} className="font-semibold text-sm text-white">E</button>
                             <button onClick={() => props.moveBlock(props.index, 'down')}>
                                 <svg className="fill-current text-gray-400 hover:text-gray-900 w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M12 14.5L17 9.5H7L12 14.5Z" />
                                 </svg>
                             </button>
                         </div>
-                        <button onClick={() => props.deleteBlock(props.index)}>
+                        {/* <button onClick={() => props.deleteBlock(props.index)}>
                             <svg className="fill-current text-gray-500 hover:text-gray-900 w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M17 22H7C5.89543 22 5 21.1046 5 20V7H3V5H7V4C7 2.89543 7.89543 2 9 2H15C16.1046 2 17 2.89543 17 4V5H21V7H19V20C19 21.1046 18.1046 22 17 22ZM7 7V20H17V7H7ZM9 4V5H15V4H9Z" />
                             </svg>
-                        </button>
-                        <button onClick={() => props.setShowEditor(true)}>Edit</button>
+                        </button> */}
                     </div>
                 </div>
+                }
                 {/* Block */}
                 <div>{props.block}</div>
             </div>
             {/* Editor */}
-            <div>
-                {props.showEditor && 
-                    <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center" style={{ background: 'rgba(0,0,0,.75)', zIndex: 100 }}>
-                        <div className="w-full max-w-screen-sm p-4 m-4 rounded-lg bg-white">
-                            <button onClick={() => props.setShowEditor(false)}>X</button>
-                            {props.form}
-                        </div>
+            {props.showEditor && 
+                <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center" style={{ background: 'rgba(0,0,0,.75)', zIndex: 100 }}>
+                    <div className="w-full max-w-screen-sm p-4 m-4 rounded-lg bg-white">
+                        <button onClick={() => props.setShowEditor(false)}>X</button>
+                        {props.form}
                     </div>
-                }
-            </div>
+                </div>
+            }
         </div>
     )
 }
